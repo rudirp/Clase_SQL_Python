5a21c2b7116eb9c1760f730d778411a16c3d093a
Rudi <rudirp.04@gmail.com>
2026-02-01 19:15:43 -0500

    Initial_Commit

---
+++ MisQueries.sql
@@ -0,0 +1,21 @@
+-- Comentario multilinea
+/*
+Ejemplo 
+Ejemplo
+Ejemplo
+*/
+
+-- 1. Definir qué BBDD se va a usar
+USE Chinook;
+
+-- 2. Seleccionar datos de tabla
+-- * equivale a todo (filas o columnas)
+
+-- Tabla
+SELECT * FROM Album;
+
+SELECT TOP 5 * FROM Album;
+
+SELECT TOP 10 * FROM Album;
+-- Vistas
+SELECT * FROM MediaType;

16556df886862385cd84d250a229d74738ef712d
Rudi <rudirp.04@gmail.com>
2026-02-03 13:00:24 -0500

    Actualización

--- MisQueries.sql
+++ MisQueries.sql
@@ -20,3 +20,39 @@ SELECT TOP 5 * FROM Album;
 SELECT TOP 10 * FROM Album;
 -- Vistas
 SELECT * FROM MediaType;
+
+-- Columnas
+SELECT * FROM Invoice;
+
+SELECT
+	BillingCity,
+	BillingCountry
+FROM Invoice;
+
+-- 3. Límite de filas
+SELECT TOP 25 * FROM Invoice;
+
+SELECT TOP 25
+	InvoiceId,
+	CustomerId,
+	BillingCity
+FROM Invoice;
+
+-- 4. Uso de alias
+SELECT
+	InvoiceId AS id_factura,
+    CustomerId AS id_cliente,
+    BillingCity AS ciudad_facturacion
+FROM Invoice;
+
+-- 5. Filtrar datos
+SELECT * FROM Invoice
+WHERE BillingCountry = 'Canada'; -- Quiero que salga canada
+
+SELECT * FROM Invoice
+WHERE BillingCountry != 'Canada'; -- No quiero que salga canada
+
+SELECT * FROM Invoice
+WHERE BillingCountry <> 'Canada'; -- No quiero que salga canada
+
+
